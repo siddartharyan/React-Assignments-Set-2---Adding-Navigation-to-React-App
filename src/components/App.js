369e225de363121e9089b88455f9a1660f19560d
siddartharyan <68399822+siddartharyan@users.noreply.github.com>
2020-11-24 16:50:10 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -15,8 +15,8 @@ function App() {
   };
   return (
     <Router>
-      <a href="/">Home</a>
-      <a href="/about">About</a>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
       <div id="main">
         <Switch>
           <Route exact default path="/" component={Home} />
