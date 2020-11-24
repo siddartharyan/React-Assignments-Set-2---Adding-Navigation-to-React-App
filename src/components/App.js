f72d9a70f74eedd2e8c5715da04da6c61f6c44aa
siddartharyan <68399822+siddartharyan@users.noreply.github.com>
2020-11-24 17:52:48 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,32 @@
 import React, { Component, useState } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import LocationDisplay from "./LocationDisplay";
+import About from "./About";
+import Home from "./Home";
+import Notfound from "./Notfound";
 import "../styles/App.css";
 
 function App() {
-  const Home = () => {
-    return <h1>You are home.</h1>;
-  };
-  const About = () => {
-    return <h1>You are on the about page.</h1>;
-  };
-  const Notfound = () => {
-    return <h1>No match</h1>;
-  };
+  // const About = () => {
+  //   return <h1>You are on the about page.</h1>;
+  // };
+  // const Home = () => {
+  //   return <h1>You are home.</h1>;
+  // };
+  // const Notfound = () => {
+  //   return <h1>No match</h1>;
+  // };
   return (
-    <Router>
+    <>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
-      <div id="main">
-        <Switch>
-          <Route exact default path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route component={Notfound} />
-        </Switch>
-        <LocationDisplay />
-      </div>
-    </Router>
+      <Switch>
+        <Route exact default path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route component={Notfound} />
+      </Switch>
+      <LocationDisplay />
+    </>
   );
 }
 
