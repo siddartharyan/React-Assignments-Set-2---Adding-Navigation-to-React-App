3ae6a41eff2e4d234f2a95a9dfdc3d5876ba8a49
siddartharyan <68399822+siddartharyan@users.noreply.github.com>
2020-11-26 18:24:43 +0530

    changes12345

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, { Component, useState } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { useLocation } from "react-router";
-import { Notfound } from "./notfound";
 import "../styles/App.css";
 import Home from "./home";
 import About from "./about";
 import LocationDisplay from "./locationDisplay";
 
 function App() {
+  const Notfound = () => {
+    return <h1>No match</h1>;
+  };
   return (
     <>
       <Router>
--- src/components/notfound.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-const Notfound = () => {
-  return <h1>No match</h1>;
-};
-export default Notfound;
